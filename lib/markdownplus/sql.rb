33c3ab8409bca830b1afcd2003b3af058d9d3700
Christopher Petersen <chris@petersen.io>
2015-03-19 07:32:17 -0700

    Intial commit

---
+++ lib/markdownplus/sql.rb
@@ -0,0 +1,43 @@
+require "markdownplus/sql/version"
+require "sequel"
+
+module Markdownplus
+  module Sql
+    class SqlHandler < Handler
+      def execute(input, parameters, warnings, errors)
+        output = nil
+        if parameters==nil
+          errors << "No connection url given"
+        elsif parameters.count == 0
+          errors << "No connection url given"
+        else
+          warnings << "More than one parameter given" if parameters.count > 1
+          if(input==nil || input.strip.empty?)
+            errors << "No sql statement given"
+          else
+            url = parameters.first
+            begin
+              database = Sequel.connect(url)
+              dataset = database[input]
+              i = 0
+              output = CSV.generate do |csv|
+                dataset.each do |row|
+                  if i==0
+                    csv << row.collect { |k,v| k }
+                  end
+                  i += 1
+                  csv << row.collect { |k,v| v }
+                end
+              end
+            rescue => e
+              output = input
+              errors << "Error opening [#{e.message}]"
+            end
+          end
+        end
+        output
+      end
+    end
+    HandlerRegistry.register("sql", SqlHandler)
+  end
+end
---
+++ lib/markdownplus/sql/version.rb
@@ -0,0 +1,5 @@
+module Markdownplus
+  module Sql
+    VERSION = "0.0.1"
+  end
+end
